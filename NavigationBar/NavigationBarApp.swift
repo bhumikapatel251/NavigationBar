593733438e86180f9b78a0473a628d43ea2b2633
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-04-05 12:29:16 +0530

    Initial Commit

---
+++ NavigationBar/NavigationBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationBarApp.swift
+//  NavigationBar
+//
+//  Created by Bhumika Patel on 05/04/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
